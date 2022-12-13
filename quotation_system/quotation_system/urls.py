952a0f5dc3c18d140c579efe7d8f8ebea7489347
Ayaan Ahmad <ayaanishere08@gmail.com>
2022-12-13 12:12:21 +0800

    Setup with templates done

--- quotation_system/quotation_system/urls.py
+++ quotation_system/quotation_system/urls.py
@@ -1,21 +1,54 @@
-"""quotation_system URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+Definition of urls for django_project1.
+"""
+
+from datetime import datetime
+from django.urls import re_path as url 
+import django.contrib.auth.views
+
+import project.forms
+import project.views
+
+import addItem.views
+
+# Uncomment the next lines to enable the admin:
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
+admin.autodiscover()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Examples:
+    url(r'^$', project.views.home, name='home'),
+    url(r'^contact$', project.views.contact, name='contact'),
+    url(r'^about$', project.views.about, name='about'),
+    url(r'^login/$',
+        django.contrib.auth.views.LoginView.as_view(),
+        {
+            'template_name': 'project/login.html',
+            'authentication_form': project.forms.BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title': 'Log in',
+                'year': datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        django.contrib.auth.views.LogoutView.as_view(),
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', admin.site.urls),
+
+    url(r'^menu$', project.views.menu, name='menu'),
+
+    ## module 1 urls
+    url(r'^additemform$', addItem.views.additemform, name="additem_form"),
+    url(r'^additemconfirmation', addItem.views.additemconfirmation, name="confirm_additem"),
 ]
