4d9e5b16d69a873c9abdc74a3a8b760a93fef313
Ayaan Ahmad <ayaanishere08@gmail.com>
2023-01-05 12:00:05 +0800

    created models

--- quotation_system/project/admin.py
+++ quotation_system/project/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from project.models import Item
+from project.models import Salesman, Customer, Manager, FinanceOfficer, PurchaseOrder, PurchaseRequisition, Quotation, PRItems, POItems, QuotationItems
 
-admin.site.register(Item)
+admin.site.register(Salesman)
+admin.site.register(Customer)
+admin.site.register(Manager)
+admin.site.register(FinanceOfficer)
+admin.site.register(PurchaseOrder)
+admin.site.register(PurchaseRequisition)
+admin.site.register(Quotation)
+admin.site.register(POItems)
+admin.site.register(PRItems)
+admin.site.register(QuotationItems)
 
--- quotation_system/project/models.py
+++ quotation_system/project/models.py
@@ -7,18 +7,97 @@ from django.db import models
 from django.contrib.auth.models import User
 
 #sharing entity
+class Salesman(models.Model):
+    salesman_id = models.CharField(primary_key=True, max_length=10)
+    salesman_password = models.CharField(max_length=5, null=True)
+    salesman_email = models.BigIntegerField(null=True)
 
-class Item(models.Model):
-    item_id = models.CharField(primary_key=True, max_length=10)
-    item_name = models.TextField()
-    item_description = models.TextField(null=True,default=None, blank=True)
     def __str__(self):
-        return str(self.item_id)
+        return str(self.salesman_id)
 
-# class User(models.Model):
-#     username =  models.TextField(null=True, blank=True)
-#     password =  models.TextField(null=True, blank=True)
-#     roles = models.TextField(null=True, blank=True)
-#     def __str__(self):
-#         return str(self.username)
+class Customer(models.Model):
+    customer_id = models.CharField(primary_key=True, max_length=10)
+    customer_name = models.CharField(max_length=20, null=True)
+    customer_email = models.BigIntegerField(null=True)
+
+    def __str__(self):
+        return str(self.customer_id)
+
+class Manager(models.Model):
+    manager_id = models.CharField(primary_key=True, max_length=10)
+    manager_name = models.CharField(max_length=20, null=True)
+    manager_email = models.BigIntegerField(null=True)
+
+    def __str__(self):
+        return str(self.manager_id)
+
+class FinanceOfficer(models.Model):
+    finance_officer_id = models.CharField(primary_key=True, max_length=10)
+    finance_officer_name = models.CharField(max_length=20, null=True)
+    finance_officer_email = models.BigIntegerField(null=True)
+
+    def __str__(self):
+        return str(self.finance_officer)
+
+
+class PurchaseRequisition(models.Model):
+    pr_id = models.CharField(primary_key=True, max_length=10)
+    customer_id = models.ForeignKey(Customer,default=None, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.product_id)
+
+class Quotation(models.Model):
+    quotation_id = models.CharField(primary_key=True, max_length=10)
+    pr_id = models.ForeignKey(PurchaseRequisition,default=None, on_delete=models.CASCADE)
+    salesman_id = models.ForeignKey(Salesman,default=None, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer,default=None, on_delete=models.CASCADE)
+    manager_id = models.ForeignKey(Manager,default=None, on_delete=models.CASCADE)
+    total_price = models.FloatField(default=None, null=True)
+    date = models.DateField()
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.quotation_id)
+
+class PurchaseOrder(models.Model):
+    po_id = models.CharField(primary_key=True, max_length=10)
+    quotation_id = models.ForeignKey(Quotation,default=None, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer,default=None, on_delete=models.CASCADE)
+    salesman_id = models.ForeignKey(Salesman,default=None, on_delete=models.CASCADE)
+    finance_officer_id = models.ForeignKey(FinanceOfficer,default=None, on_delete=models.CASCADE)
+    total_price = models.FloatField(default=None, null=True)
+    date = models.DateField()
     
+    def __str__(self):
+        return str(self.po_id)
+
+class PRItems(models.Model):
+    pr_item_id = models.CharField(primary_key=True, max_length=10)
+    pr_id = models.ForeignKey(PurchaseRequisition, default=None, on_delete=models.CASCADE)
+    pr_item_name = models.CharField(max_length=40, null=True)
+    pr_item_quantity = models.PositiveIntegerField(default=None, null=True)
+
+    def __str__(self):
+        return str(self.pr_item_id)
+
+class QuotationItems(models.Model):
+    q_item_id = models.CharField(primary_key=True, max_length=10)
+    quotation_id = models.ForeignKey(Quotation, default=None, on_delete=models.CASCADE)
+    q_item_name = models.CharField(max_length=40, null=True)
+    q_item_quantity = models.PositiveIntegerField(default=None, null=True)
+    q_item_price = models.FloatField(default=None, null=True)
+
+    def __str__(self):
+        return str(self.q_item_id)
+
+class POItems(models.Model):
+    po_item_id = models.CharField(primary_key=True, max_length=10)
+    po_id = models.ForeignKey(PurchaseOrder,default=None, on_delete=models.CASCADE)
+    po_item_name = models.CharField(max_length=40, null=True)
+    po_item_quantity = models.PositiveIntegerField(default=None, null=True)
+    po_item_price = models.FloatField(default=None, null=True)
+
+    def __str__(self):
+        return str(self.po_item_id)
