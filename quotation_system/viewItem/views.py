a7583457122593af741dc91de36f986ee10582fd
Ayaan Ahmad <ayaanishere08@gmail.com>
2023-01-18 13:48:24 +0800

    done with everything

--- quotation_system/viewItem/views.py
+++ quotation_system/viewItem/views.py
@@ -1,9 +1,10 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from project.models import QuotationItems, Quotation, Salesman, Customer, FinanceOfficer, PurchaseOrder, POItems, \
     PurchaseRequisition, PRItems
 from addItem.forms import QuotationForm
 from django.views.decorators.csrf import csrf_exempt
+
 
 def view_purchase_requisition(request):
     Pr = PurchaseRequisition.objects.all()
@@ -50,18 +51,14 @@ def view_one_PR(request, pr_id):
 
 
 def view_all_quotation(request):
-    Quotations = Quotation.objects.all().values()
-    Quotation_item = QuotationItems.objects.all().values()
-
     is_customer = request.user.groups.filter(name='Customer').exists()
     is_manager = request.user.groups.filter(name='Manager').exists()
-    if(is_manager):
+    if (is_customer):
+        Quotations = Quotation.objects.filter(status__in=['Accepted', 'Rejected', 'Approved']).values()
+    else:
         Quotations = Quotation.objects.all().values()
-        Quotation_item = QuotationItems.objects.all().values()
-    elif(is_customer):
-       #only cleared status should be here
-        Quotations = Quotation.objects.all().values()
-        Quotation_item = QuotationItems.objects.all().values()
+
+    Quotation_item = QuotationItems.objects.all().values()
 
     print(Quotation_item)
     context = {
@@ -93,6 +90,8 @@ def view_active_quotation(request):
     Quotations = Quotation.objects.all().values()
     Quotation_item = QuotationItems.objects.all().values()
 
+    Quotations = Quotation.objects.filter(status__in=['Uncleared', 'Rejected', 'Approved']).values()
+
     print(Quotation_item)
     context = {
         'Quotations': Quotations,
@@ -109,29 +108,46 @@ def view_one_quotation(request, quotation_id):
     is_customer = request.user.groups.filter(name='Customer').exists()
     is_manager = request.user.groups.filter(name='Manager').exists()
 
-    Quotation_item = QuotationItems.objects.filter(quotation_id=select_q_id)
+    current_status = Quotation.objects.get(quotation_id=quotation_id).status
 
-    print(Quotation_item)
+    Quotation_item = QuotationItems.objects.filter(quotation_id=select_q_id)
+    is_approved = False
+    if current_status == 'Approved':
+        is_approved = True
+        print(is_approved)
+    else:
+        is_approved = False
+    print(current_status)
     context = {
         'selected_quotation': select_q_id,
         'Quotation_item': Quotation_item,
         'salesman': is_salesman,
         'customer': is_customer,
+        'is_approved': is_approved,
         'manager': is_manager,
     }
     return render(request, 'Quotation/viewOneQuotation.html', context)
 
+
 @csrf_exempt
 def update_quotation(request):
-    quotation_id = request.GET.get('quotation_id')
-    print(quotation_id)
+    if request.method == 'POST':
+        quotation_id = request.POST.get('quotation_id')
+        status_type = request.POST.get('status_type')
 
-    if request.method == 'GET':
-        form = QuotationForm(request.POST)
-        Quotation.objects.filter(quotation_id).update(status='Accepted')
-        print(form)
+        print(quotation_id + "," + status_type)
+        if status_type == 'Accepted':
+            Quotation.objects.filter(quotation_id=quotation_id).update(status='Accepted')
+        elif status_type == 'Approved':
+            Quotation.objects.filter(quotation_id=quotation_id).update(status='Approved')
+        elif status_type == 'Declined':
+            Quotation.objects.filter(quotation_id=quotation_id).update(status='Declined')
+        elif status_type == 'Rejected':
+            Quotation.objects.filter(quotation_id=quotation_id).update(status='Rejected')
+        return JsonResponse("Status saved", safe=False)
+    else:
+        return HttpResponseBadRequest("Invalid request method")
 
-    return JsonResponse("Status saved", safe=False)
 
 def view_PO(request):
     fo_id = FinanceOfficer.objects.get(user=request.user).finance_officer_id
