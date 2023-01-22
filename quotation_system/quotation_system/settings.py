27ca97ad320f6ecc5f6ebd114bb03f1336010627
Ayaan Ahmad <ayaanishere08@gmail.com>
2023-01-22 17:32:01 +0800

    Update settings.py

--- quotation_system/quotation_system/settings.py
+++ quotation_system/quotation_system/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'project',
     'addItem',
     'viewItem',
+    'view_breadcrumbs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
